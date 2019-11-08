a6efe72992dff0fb38247e83b7e92fbfe93733e2
Dan Hogan <hogan.daniel@hotmail.com>
2019-11-07 19:26:43 -0800

    create user model for db

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const Bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
